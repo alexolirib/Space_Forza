2d35c3e276143d98058a40459c252994f2cad524
Alexandre Ribeiro <alexolirib@gmail.com>
2019-03-19 09:05:54 -0300

    criado os asteroid

--- main.lua
+++ main.lua
@@ -5,16 +5,51 @@ physics.setGravity( 0, 0 )
 
 math.randomseed(os.time())
 
+local score = 0
+
+local gameLoopTimer
+local scoreText
+
 
 local rocket_name = 'power_force_one'
 
 local backGroup = display.newGroup() 
 local mainGroup = display.newGroup()
 
+local sheetOptions =
+{
+    frames =
+    {
+        {   -- 1) asteroid 1
+            x = 0,
+            y = 0,
+            width = 102,
+            height = 85
+        },
+        {   -- 2) asteroid 2
+            x = 0,
+            y = 85,
+            width = 90,
+            height = 83
+        },
+        {   -- 3) asteroid 3
+            x = 0,
+            y = 168,
+            width = 100,
+            height = 97
+        },
+    }, 
+}
 
-local background = display.newImageRect( backGroup, "image/background3.jpg", 360, 570 )
+local objectSheet = graphics.newImageSheet( "image/gameObjects.png", sheetOptions )
+
+
+local background = display.newImageRect( backGroup, "image/background3.jpg", 360, 800 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
+
+transition.moveBy(background, {y=1000, time=500001})
+
 
 rocket = display.newImageRect(mainGroup, "./image/power_force_one.png",60,60)
 rocket.x = display.contentCenterX
@@ -22,6 +57,16 @@ rocket.y = display.contentHeight - 60
 
 physics.addBody( rocket, { radius=30, isSensor=true } )
 rocket.myName = rocket_name
+
+scoreText = display.newText( "Score: " .. score, 240, 25, native.systemFont, 16 )
+
+scoreText:setFillColor(0.180, 0.65, 0.35  )
+
+
+local function updateText()
+    scoreText.text = "Score: " .. score
+end
+
 
 
 local function dragRocket( event )
@@ -48,6 +93,38 @@ local function dragRocket( event )
     return true
 end
 
+
+local asteroidTable = {}
+
+local function createAsteroid()
+    
+    local newAsteroid = display.newImageRect( mainGroup, objectSheet, 1, 102, 10 )
+    physics.addBody( newAsteroid, "dynamic", { radius=40, bounce=0.8 } )
+    newAsteroid.myName = "asteroid"
+
+    
+    newAsteroid:toBack()
+    table.insert(asteroidTable, newAsteroid)
+    physics.addBody(newAsteroid, 'dynamic', {isSensor = true})
+
+    newAsteroid.x = math.random( display.contentWidth )
+    newAsteroid.y = 5
+    newAsteroid:setLinearVelocity( math.random( -30,20 ), math.random( 120,190 ) )
+end
+
+
+local function gameLoop()
+    createAsteroid()
+    score = score + 2
+    updateText()
+end
+
+
+local temp = 750
+
+
+
+gameLoopTimer = timer.performWithDelay( temp, gameLoop, 0 )
 
 
 rocket:addEventListener( "touch", dragRocket )
